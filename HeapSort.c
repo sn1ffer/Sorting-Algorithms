53ecc026833fef0f354623ccdfff14f3fa931685
mvmikhaylova <91223620+mvmikhaylova@users.noreply.github.com>
2022-06-25 16:39:55 +0300

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
 
+void siftDown(int *numbers, int root, int bottom){
+    int maxChild;
+    int done = 0;
+    while ((root * 2 <= bottom) && (!done)){
+        if (root * 2 == bottom)
+            maxChild = root * 2;
+        else if (numbers[root * 2] > numbers[root * 2 + 1])
+            maxChild = root * 2;
+        else
+            maxChild = root * 2 + 1;
+        if (numbers[root] < numbers[maxChild]){
+            int temp = numbers[root];
+            numbers[root] = numbers[maxChild];
+            numbers[maxChild] = temp;
+            root = maxChild;
+        }
+        else
+            done = 1;
+    }
+}
+
+void heapSort(int *numbers, int array_size){
+    for (int i = (array_size / 2); i >= 0; i--)
+        siftDown(numbers, i, array_size - 1);
+
+    for (int i = array_size - 1; i >= 1; i--){
+        int temp = numbers[0];
+        numbers[0] = numbers[i];
+        numbers[i] = temp;
+        siftDown(numbers, 0, i - 1);
+    }
+}
+
+
+int main(){
+
+    int count;
+    printf("Enter the size of the array: ");
+    scanf("%d", &count);
+    printf("Enter the array elements: ");
+    int arr[count];
+    for (int i=0; i < count;i++) {
+        scanf("%d", &arr[i]);
+    }
+    heapSort(arr, count);
+    printf("Result: ");
+    for(int i=0;i<count;i++) {
+        printf(" %d", arr[i], " ");
+    }
+};
